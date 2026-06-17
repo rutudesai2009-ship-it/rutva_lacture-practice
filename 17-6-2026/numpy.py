53255a53ca447bc3d88eb101332f9a0b012ab325
rutudesai2009-ship-it <rutudesai2009@gmail.com>
2026-06-17 18:08:11 +0530

    Add files via upload

---
+++ 17-6-2026/numpy.py
@@ -0,0 +1,31 @@
+# What is numpy ?
+
+'''
+Numpy (Numerical pyton) is a powerful pythonn librery used for numerical computing. It provides suppart for multi-dimensional array
+matrices,and mathemetical function
+'''
+
+# Fast and efficient array operations.
+# Multi-dimensional arrats.
+# Methemetical and statisical functions.
+# Less memory consumption than python list
+# Used to Data Science , machine Lerning , and AI
+
+# Install Numpy
+
+# pip install numpy
+
+# python-m pip install numpy
+
+# Import numpy
+
+import numpy as np
+
+arr=np.arrray([10,20,30,40,50])
+
+print(arr)
+
+# Install Jupyter Notebook
+
+# Jupyter notebook is an open - source web application used to write and execute python code . It is widley used for Data science , Machine Larning and Data Analises.
+
